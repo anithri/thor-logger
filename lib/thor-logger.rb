f8cdb4d98cdfa4e551351789bd3cad9ad82878bd
Scott M Parrish <anithri@gmail.com>
2013-07-16 20:58:03 -0600

    notes to redo using shell

--- lib/thor-logger.rb
+++ lib/thor-logger.rb
@@ -1,55 +1,82 @@
-require 'thor'
-require 'log4r'
-require 'colorize'
+class Thor
+  module Shell
+    class BasicLogger < Basic
+      alias_method :say_status_orig, :say_status
 
-module ThorLogger
-  def logger
+      def say_status(status, message, log_status = true)
+        logger.send(status, message)
+        super(status, message, log_status)
+      end
 
+      def logger
+        @@logger
+      end
+    end
+
+    class ColorLogger < Color
+      alias_method :say_status_orig, :say_status
+
+      def say_status(status, message, log_status = true)
+        logger.send(status, message)
+        super(status, message, log_status)
+      end
+
+      def logger
+        raise
+        @@logger
+      end
+    end
+  end
+end
+
+require 'logger'
+module Lightning
+  class Logger
+
+    def self.setup(filename = "/tmp/lightning.txt", log_level = ::Logger::INFO)
+      logger = ::Logger.new(filename)
+      logger.level = log_level
+      logger.datetime_format = '%Y-%m-%d %H:%M:%S'
+      logger.formatter = proc do |severity, datetime, prog_name, msg|
+        "#{datetime.strftime(logger.datetime_format)} #{severity}: #{msg}\n"
+      end
+
+      logger
+    end
   end
 
-  def self.included(base)
+end
 
-    base.class_option :color,   type:  :boolean,
-                                desc:  "Output to screen in ANSI color?",
-                                group:  :output,
-                                default:  true
+class Thor
+  module Shell
+    class BasicLogger < Basic
+      alias_method :say_status_orig, :say_status
+      def say_status(status, message, log_status = true)
+        spaces = "  " * padding
+        $logger.send(status, spaces + message)
+        say_status_orig(status, message, color_for(status, log_status))
+      end
 
-    base.class_option :logfile, type:  :string,
-                                desc:  "Record Log info to FILE",
-                                group:  :output,
-                                aliases:  '-l'
+      def error(statement)
+        say_status(:error, statement, :red) if @base.output?
+        exit(1)
+      end
 
-    base.class_option :loglevel,type:  :string,
-                                desc:  "Explicitly set Log Level[]",
-                                group:  :output,
-                                default:  :info
-
-    base.class_option :silent,  type:  :boolean,
-                                desc:  "Output only Fatal messages to console",
-                                group:  :output,
-                                default:  false,
-                                aliases:  '-s'
-
-    base.class_option :quiet,   type:  :boolean,
-                                desc:  "Be less verbose. Ignored when --loglevel set",
-                                group:  :output,
-                                default:  false,
-                                aliases:  '-q'
-
-    base.class_option :verbose, type:  :boolean,
-                                desc:  "Be more verbose. Ignored when --loglevel set",
-                                group:  :output,
-                                default:  false,
-                                aliases:  '-v'
-
-    base.class_option :debug,   type:  :boolean,
-                                desc:  "Output extra info to aid debugging. Ignored when --loglevel set",
-                                group:  :output,
-                                default: false
+      def color_for(status, log_status)
+        return log_status unless log_status
+        case status.to_s
+          when "debug"
+            :blue
+          when "info"
+            :green
+          when "warn"
+            :yellow
+          when "error"
+            :red
+          else
+            log_status
+        end
+      end
+    end
   end
-
-  def setup_logger(options)
-    warn options.inspect
-  end
-
 end
